ac5023ebad5baa6a1820a22b5f50e50218bd7a26
Nikita Ivaniushchenko <nikita@ngti.nl>
2016-12-20 17:39:37 +0100

    setting up carthage compatibility

---
+++ CCHMapClusterController Example iOS/CCCHMapCluster/CCCHMapCluster.h
@@ -0,0 +1,19 @@
+//
+//  CCCHMapCluster.h
+//  CCCHMapCluster
+//
+//  Created by Nikita Ivaniushchenko on 12/20/16.
+//  Copyright © 2016 Claus Höfele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCCHMapCluster.
+FOUNDATION_EXPORT double CCCHMapClusterVersionNumber;
+
+//! Project version string for CCCHMapCluster.
+FOUNDATION_EXPORT const unsigned char CCCHMapClusterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCHMapCluster/PublicHeader.h>
+
+
---
+++ CCHMapClusterController Example iOS/CCHMapCluster/CCHMapCluster.h
@@ -0,0 +1,19 @@
+//
+//  CCHMapCluster.h
+//  CCHMapCluster
+//
+//  Created by Nikita Ivaniushchenko on 12/20/16.
+//  Copyright © 2016 Claus Höfele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCHMapCluster.
+FOUNDATION_EXPORT double CCHMapClusterVersionNumber;
+
+//! Project version string for CCHMapCluster.
+FOUNDATION_EXPORT const unsigned char CCHMapClusterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCHMapCluster/PublicHeader.h>
+
+
